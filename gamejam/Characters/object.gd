50e9e01e41c63418c53228b24116a5f431581fd4
gencsis <149344244+gencsis@users.noreply.github.com>
2026-02-22 00:05:43 -0600

    connect horse game to car game

--- gamejam/Characters/object.gd
+++ gamejam/Characters/object.gd
@@ -7,4 +7,4 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://horse_level.tscn")
